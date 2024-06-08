82261e8c31fab24037eacd04e1af1612858428a9
Phil <phil.github@gmail.com>
2024-06-08 19:35:34 +0300

    done

--- lib/debug.py
+++ lib/debug.py
@@ -4,6 +4,23 @@
 from __init__ import CONN, CURSOR
 from department import Department
 
-import ipdb
+# import ipdb
 
-ipdb.set_trace()
+Department.drop_table()
+Department.create_table()
+
+payroll = Department.create("Payroll", "Building A, 5th Floor")
+print(payroll)  # <Department 1: Payroll, Building A, 5th Floor>
+
+hr = Department.create("Human Resources", "Building C, East Wing")
+print(hr)
+
+hr.name = 'HR'
+hr.location = "Building F, 10th Floor"
+hr.update()
+print(hr)
+
+print("Delete Payroll")
+payroll.delete()
+print(payroll)
+# ipdb.set_trace()
--- lib/department.py
+++ lib/department.py
@@ -10,3 +10,58 @@ class Department:
 
     def __repr__(self):
         return f"<Department {self.id}: {self.name}, {self.location}>"
+    
+
+    @classmethod
+    def create_table(cls):
+        # create a new table to persist the attributes of Department Instances
+        sql = """
+            CREATE TABLE IF NOT EXISTS departments(
+              id INTEGER PRIMARY KEY AUTOINCREMENT,
+              name TEXT,
+              location TEXT)
+        """
+        CURSOR.execute(sql)
+        CONN.commit()
+
+    @classmethod
+    def drop_table(cls):
+        sql = """
+            DROP TABLE IF EXISTS departments
+        """
+        CURSOR.execute(sql)
+        CONN.commit()
+    
+    @classmethod
+    def create(cls, name, location):
+        """ Initialize a new Department instance and save the object to the database """
+        department = cls(name, location)
+        department.save()
+        return department
+    
+    def save(self):
+        sql = """
+            INSERT INTO departments (name, location) 
+            VALUES(?, ?)
+        """
+        CURSOR.execute(sql, (self.name, self.location))
+        CONN.commit()
+
+        self.id = CURSOR.lastrowid
+    
+    def update(self):
+        sql = """
+            UPDATE departments
+            SET name = ?, location = ?
+            WHERE id = ?
+        """
+        CURSOR.execute(sql, (self.name, self.location, self.id))
+        CONN.commit()
+
+    def delete(self):
+        sql = """
+            DELETE FROM departments
+            WHERE id = ?
+        """
+        CURSOR.execute(sql, (self.id,))
+        CONN.commit()
